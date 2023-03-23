98f15c0a6a0a3d23ff013614cfc41932c18ca03e
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-03-23 04:13:02 -0700

    lol

--- monty.c
+++ monty.c
@@ -8,26 +8,25 @@
  */
 instruction_t get_opcode(char *line)
 {
-        int i;
-        char *opcode;
-        instruction_t opcode_func;
+	int i;
+	char *opcode;
+	instruction_t opcode_func;
 
-        opcode_func.f = NULL;
-        opcode = strtok(line, " \n\t");
+	opcode_func.f = NULL;
+	opcode = strtok(line, " \n\t");
 
-        if (!opcode || opcode[0] == '#')
-                return (opcode_func);
+	if (!opcode || opcode[0] == '#')
+		return (opcode_func);
 
-        for (i = 0; opcode_list[i].opcode != NULL; i++)
-        {
-                if (strcmp(opcode_list[i].opcode, opcode) == 0)
-                {
-                        opcode_func = opcode_list[i];
-                        break;
-                }
-        }
-
-        return (opcode_func);
+	for (i = 0; opcode_list[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode_list[i].opcode, opcode) == 0)
+		{
+			opcode_func = opcode_list[i];
+			break;
+		}
+	}
+	return (opcode_func);
 }
 
 /**
@@ -38,12 +37,12 @@ instruction_t get_opcode(char *line)
  */
 void free_stack(stack_t *stack)
 {
-        stack_t *temp;
+	stack_t *temp;
 
-        while (stack)
-        {
-                temp = stack->next;
-                free(stack);
-                stack = temp;
-        }
+	while (stack)
+	{
+		temp = stack->next;
+		free(stack);
+		stack = temp;
+	}
 }
